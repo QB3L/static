e811ca2cd1548cb418ea90daabd8cf2214bf236e
Ruben Nieves <rnieves@vmware.com>
2017-10-06 20:40:58 -0500

    no autocapitalize on text field

--- apps/main.js
+++ apps/main.js
@@ -7,7 +7,7 @@ const app = new Vue({
     },
     template: `
     <div>
-        <input v-model="ngroklink" class="style-1" type="text" id="custom-link" />
+        <input v-model="ngroklink" class="style-1" type="text" id="custom-link" autocapitalize="none" />
         <div class="link">
             <a :href="'boxer-feature://BoxerEnsEnabledForPush?signature=e1df6749968518e89521c7a5aa2253d1&url=http%3A%2F%2F' + ngroklink + '.ngrok.io%2FMailNotificationService%2Fapi%2Fens'">
                 Enable entered ngrok ENS
