e5cbbfa30a0011dbf85d3d585ffbd1a490c89aea
Ruben Mini <rubennieves@pplsi.com>
2023-10-26 18:28:14 -0500

    screw it, going with react and vite

--- tailwind/components.js
+++ tailwind/components.js
@@ -3,18 +3,11 @@ function createCustomElement(templateId, elementName, callback) {
 		constructor() {
 			super();
 			console.log('constructing', elementName);
-			/* Appending to Shadow DOM
+			/* Appending to Shadow DOM */
 			const shadow = this.attachShadow({ mode: 'open' });
 			const template = document.getElementById(templateId);
 			const instance = document.importNode(template.content, true);
 			shadow.appendChild(instance);
-      */
-
-			// Another approach
-			const template = document.getElementById(templateId);
-			const clone = template.content.cloneNode(true);
-			this.appendChild(clone);
-
 			callback(shadow, templateId);
 		}
 		connectedCallback() {
